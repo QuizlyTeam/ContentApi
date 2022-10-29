07d11d9fb90ea24e9020a0a81b67f6f336246cfb
Michal <michalszc05@gmail.com>
2022-10-29 22:36:15 +0200

    Minor changes

--- api/routes/teapot.py
+++ api/routes/teapot.py
@@ -4,7 +4,7 @@ router = APIRouter()
 
 
 @router.get("/teapot", status_code=418)
-def teapot():
+def teapot(): # ☕
     """
     Error response code indicates that the server refuses to brew coffee because it is, permanently, a teapot.
     """  # noqa: E501
