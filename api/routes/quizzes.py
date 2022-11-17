e04ca19f943be74e682f34397f4f97c71ca994b9
Tosiek <antonizieciak@gmail.com>
2022-11-17 23:10:26 +0100

    Created documentation for tags.

--- api/routes/quizzes.py
+++ api/routes/quizzes.py
@@ -34,3 +34,33 @@ async def get_categories():
     categories = list(result.json().keys())
 
     return {"categories": categories}
+
+
+@router.get(
+    "/tags",
+    response_model=CategoriesModel,
+    responses={
+        status.HTTP_200_OK: {
+            "description": "Successful Response",
+            "content": {
+                "application/json": {
+                    "example": [
+                        "alcohol",
+                        "acting"
+                    ]
+                }
+            },
+        },
+        status.HTTP_500_INTERNAL_SERVER_ERROR: {
+            "description": "Something went wrong"
+        },
+    },
+)
+async def get_tags():
+    """
+    Get quiz categories.
+    """
+    result = get(f"{settings.server.quiz_api}/tags")
+    categories = list(result.json().keys())
+
+    return {"categories": categories}
