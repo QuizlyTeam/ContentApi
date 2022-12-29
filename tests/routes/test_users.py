2eb5efc887ac83a1b7ccc44c6ae7d94a6df81f16
Michal <michalszc05@gmail.com>
2022-12-29 22:01:47 +0100

    Added tests for user routes

---
+++ tests/routes/test_users.py
@@ -0,0 +1,394 @@
+from collections import OrderedDict
+from firebase_admin.exceptions import FirebaseError
+
+
+def test_post_user_success(mocker, api_client):
+    mocker.patch(
+        "firebase_admin.auth.verify_id_token",
+        return_value={
+            "uid": "XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+            "name": "Alan Turing",
+            "picture": "http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcQCUUTlwrAsh5cZ9yds4YIELmAgLT-kyXXJ7vhrzQLriwYb3VQqSTToKpq7heaNcMZX",  # noqa
+        },
+    )
+
+    expected_result = {
+        "uid": "XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        "name": "Alan Turing",
+        "nickname": "turingComplete",
+        "picture": "http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcQCUUTlwrAsh5cZ9yds4YIELmAgLT-kyXXJ7vhrzQLriwYb3VQqSTToKpq7heaNcMZX",  # noqa
+        "win": 0,
+        "lose": 0,
+        "favourite_category": "-",
+        "max_points": 0,
+    }
+
+    class MockDB:
+        def __init__(self):
+            pass
+
+        def order_by_child(self, arg):
+            return self
+
+        def equal_to(self, arg):
+            return self
+
+        def limit_to_first(self, arg):
+            return self
+
+        def get(self):
+            return None
+
+        def push(self):
+            return self
+
+        def set(self, arg):
+            pass
+
+    mocker.patch("firebase_admin.db.reference", return_value=MockDB())
+
+    response = api_client.post(
+        "/v1/users/",
+        headers={"Authorization": "Bearer firebaserusertokenid"},
+        json={"nickname": "turingComplete"},
+    )
+
+    assert response.status_code == 201
+    result = response.json()
+    assert result == expected_result
+
+
+def test_post_user_failure_unauthorized_no_credential(mocker, api_client):
+    expected_result = {"detail": "Bearer authentication is needed"}
+
+    response = api_client.post(
+        "/v1/users/", json={"nickname": "turingComplete"}
+    )
+
+    assert response.status_code == 401
+    result = response.json()
+    assert result == expected_result
+
+
+def test_post_user_failure_unauthorized_invalid_credential(mocker, api_client):
+    def verify_id_token(arg):
+        raise FirebaseError(code=401, message="")
+
+    mocker.patch("firebase_admin.auth.verify_id_token", verify_id_token)
+
+    expected_result = {"detail": "Invalid authentication from Firebase. "}
+
+    response = api_client.post(
+        "/v1/users/",
+        headers={"Authorization": "Bearer firebaserusertokenid"},
+        json={"nickname": "turingComplete"},
+    )
+
+    assert response.status_code == 401
+    result = response.json()
+    assert result == expected_result
+
+
+def test_post_user_failure_account(mocker, api_client):
+    mocker.patch(
+        "firebase_admin.auth.verify_id_token",
+        return_value={
+            "uid": "XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+            "name": "Alan Turing",
+            "picture": "http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcQCUUTlwrAsh5cZ9yds4YIELmAgLT-kyXXJ7vhrzQLriwYb3VQqSTToKpq7heaNcMZX",  # noqa
+        },
+    )
+
+    expected_result = {"detail": "Account already exists"}
+
+    class MockDB:
+        def __init__(self):
+            pass
+
+        def order_by_child(self, arg):
+            return self
+
+        def equal_to(self, arg):
+            return self
+
+        def limit_to_first(self, arg):
+            return self
+
+        def get(self):
+            return OrderedDict(
+                [
+                    (
+                        "XXXXXXXXXXXXXXXXXXXX",
+                        {
+                            "uid": "XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+                            "name": "Alan Turing",
+                            "nickname": "turingComplete",
+                            "picture": "http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcQCUUTlwrAsh5cZ9yds4YIELmAgLT-kyXXJ7vhrzQLriwYb3VQqSTToKpq7heaNcMZX",  # noqa
+                            "win": 0,
+                            "lose": 0,
+                            "favourite_category": "-",
+                            "max_points": 0,
+                        },
+                    )
+                ]
+            )
+
+        def push(self):
+            return self
+
+        def set(self, arg):
+            pass
+
+    mocker.patch("firebase_admin.db.reference", return_value=MockDB())
+
+    response = api_client.post(
+        "/v1/users/",
+        headers={"Authorization": "Bearer firebaserusertokenid"},
+        json={"nickname": "turingComplete"},
+    )
+
+    assert response.status_code == 409
+    result = response.json()
+    assert result == expected_result
+
+
+def test_post_user_failure_nickname(mocker, api_client):
+    mocker.patch(
+        "firebase_admin.auth.verify_id_token",
+        return_value={
+            "uid": "XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+            "name": "Alan Turing",
+            "picture": "http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcQCUUTlwrAsh5cZ9yds4YIELmAgLT-kyXXJ7vhrzQLriwYb3VQqSTToKpq7heaNcMZX",  # noqa
+        },
+    )
+
+    expected_result = {"detail": "Nickname turingComplete was already taken"}
+
+    class MockDB:
+        def __init__(self):
+            self.order = ""
+
+        def order_by_child(self, arg):
+            self.order = arg
+            return self
+
+        def equal_to(self, arg):
+            return self
+
+        def limit_to_first(self, arg):
+            return self
+
+        def get(self):
+            if self.order == "uid":
+                return None
+            else:
+                return OrderedDict(
+                    [
+                        (
+                            "YYYYYYYYYYYYYYYYYYYY",
+                            {
+                                "uid": "YYYYYYYYYYYYYYYYYYYYYYYYYYYY",
+                                "name": "Alan Turing",
+                                "nickname": "turingComplete",
+                                "picture": "http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcQCUUTlwrAsh5cZ9yds4YIELmAgLT-kyXXJ7vhrzQLriwYb3VQqSTToKpq7heaNcMZX",  # noqa
+                                "win": 0,
+                                "lose": 0,
+                                "favourite_category": "-",
+                                "max_points": 0,
+                            },
+                        )
+                    ]
+                )
+
+        def push(self):
+            return self
+
+        def set(self, arg):
+            pass
+
+    mocker.patch("firebase_admin.db.reference", return_value=MockDB())
+
+    response = api_client.post(
+        "/v1/users/",
+        headers={"Authorization": "Bearer firebaserusertokenid"},
+        json={"nickname": "turingComplete"},
+    )
+
+    assert response.status_code == 409
+    result = response.json()
+    assert result == expected_result
+
+
+def test_post_user_failure_validation(mocker, api_client):
+    mocker.patch(
+        "firebase_admin.auth.verify_id_token",
+        return_value={
+            "uid": "XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+            "name": "Alan Turing",
+            "picture": "http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcQCUUTlwrAsh5cZ9yds4YIELmAgLT-kyXXJ7vhrzQLriwYb3VQqSTToKpq7heaNcMZX",  # noqa
+        },
+    )
+
+    response = api_client.post(
+        "/v1/users/",
+        headers={"Authorization": "Bearer firebaserusertokenid"},
+        json={},
+    )
+
+    assert response.status_code == 422
+
+
+def test_post_user_failure_firebase(mocker, api_client):
+    mocker.patch(
+        "firebase_admin.auth.verify_id_token",
+        return_value={
+            "uid": "XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+            "name": "Alan Turing",
+            "picture": "http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcQCUUTlwrAsh5cZ9yds4YIELmAgLT-kyXXJ7vhrzQLriwYb3VQqSTToKpq7heaNcMZX",  # noqa
+        },
+    )
+
+    class MockDB:
+        def __init__(self):
+            pass
+
+        def order_by_child(self, arg):
+            raise FirebaseError(code=503, message="error")
+
+    mocker.patch("firebase_admin.db.reference", return_value=MockDB())
+
+    response = api_client.post(
+        "/v1/users/",
+        headers={"Authorization": "Bearer firebaserusertokenid"},
+        json={"nickname": "turingComplete"},
+    )
+
+    assert response.status_code == 500
+
+
+def test_get_user_success(mocker, api_client):
+    mocker.patch(
+        "firebase_admin.auth.verify_id_token",
+        return_value={"uid": "XXXXXXXXXXXXXXXXXXXXXXXXXXXX"},
+    )
+
+    expected_result = {
+        "uid": "XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        "name": "Alan Turing",
+        "nickname": "turingComplete",
+        "picture": "http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcQCUUTlwrAsh5cZ9yds4YIELmAgLT-kyXXJ7vhrzQLriwYb3VQqSTToKpq7heaNcMZX",  # noqa
+        "win": 0,
+        "lose": 0,
+        "favourite_category": "-",
+        "max_points": 0,
+    }
+
+    class MockDB:
+        def __init__(self):
+            pass
+
+        def order_by_child(self, arg):
+            return self
+
+        def equal_to(self, arg):
+            return self
+
+        def limit_to_first(self, arg):
+            return self
+
+        def get(self):
+            return OrderedDict([("XXXXXXXXXXXXXXXXXXXX", expected_result)])
+
+    mocker.patch("firebase_admin.db.reference", return_value=MockDB())
+
+    response = api_client.get(
+        "/v1/users/", headers={"Authorization": "Bearer firebaserusertokenid"}
+    )
+
+    assert response.status_code == 200
+    result = response.json()
+    assert result == expected_result
+
+
+def test_get_user_failure_unauthorized_no_credential(mocker, api_client):
+    expected_result = {"detail": "Bearer authentication is needed"}
+
+    response = api_client.get("/v1/users/")
+
+    assert response.status_code == 401
+    result = response.json()
+    assert result == expected_result
+
+
+def test_get_user_failure_unauthorized_invalid_credential(mocker, api_client):
+    def verify_id_token(arg):
+        raise FirebaseError(code=401, message="")
+
+    mocker.patch("firebase_admin.auth.verify_id_token", verify_id_token)
+
+    expected_result = {"detail": "Invalid authentication from Firebase. "}
+
+    response = api_client.get(
+        "/v1/users/", headers={"Authorization": "Bearer firebaserusertokenid"}
+    )
+
+    assert response.status_code == 401
+    result = response.json()
+    assert result == expected_result
+
+
+def test_get_user_failure_not_found(mocker, api_client):
+    mocker.patch(
+        "firebase_admin.auth.verify_id_token",
+        return_value={"uid": "XXXXXXXXXXXXXXXXXXXXXXXXXXXX"},
+    )
+
+    expected_result = {"detail": "User cannot be found"}
+
+    class MockDB:
+        def __init__(self):
+            pass
+
+        def order_by_child(self, arg):
+            return self
+
+        def equal_to(self, arg):
+            return self
+
+        def limit_to_first(self, arg):
+            return self
+
+        def get(self):
+            return None
+
+    mocker.patch("firebase_admin.db.reference", return_value=MockDB())
+
+    response = api_client.get(
+        "/v1/users/", headers={"Authorization": "Bearer firebaserusertokenid"}
+    )
+
+    assert response.status_code == 404
+    result = response.json()
+    assert result == expected_result
+
+
+def test_get_user_failure_firebase(mocker, api_client):
+    mocker.patch(
+        "firebase_admin.auth.verify_id_token",
+        return_value={"uid": "XXXXXXXXXXXXXXXXXXXXXXXXXXXX"},
+    )
+
+    class MockDB:
+        def __init__(self):
+            pass
+
+        def order_by_child(self, arg):
+            raise FirebaseError(code=503, message="error")
+
+    mocker.patch("firebase_admin.db.reference", return_value=MockDB())
+
+    response = api_client.get(
+        "/v1/users/", headers={"Authorization": "Bearer firebaserusertokenid"}
+    )
+
+    assert response.status_code == 500
