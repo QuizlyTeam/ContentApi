d16731541d52f8d818ddf356ac7b6fcd8edd88b2
Michal <michalszc05@gmail.com>
2022-12-30 12:00:02 +0100

    Added points function

---
+++ api/utils/points.py
@@ -0,0 +1,8 @@
+def points_function(t: float) -> float:
+    """
+    Return points equivalent to time spend on answer.
+
+    :param t: Time spend on answer.
+    :return: Points.
+    """
+    return round(8.31828329*1.2**(-(t-12.23262668))/0.13756839-62.72982658)
